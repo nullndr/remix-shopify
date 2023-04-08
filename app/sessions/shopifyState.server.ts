5238ad58509729b408f88b0be733c64900521195
Andrea <andrea@yaaaw.it>
2023-04-08 07:42:57 +0200

    chore: update cookie session

--- app/sessions/shopifyState.server.ts
+++ app/sessions/shopifyState.server.ts
@@ -3,11 +3,11 @@ import { createCookieSessionStorage } from "@remix-run/node";
 export const shopifyState = createCookieSessionStorage({
   cookie: {
     name: "shopify state",
-    secure: process.env.NODE_ENV !== "development",
+    secure: true,
     secrets: ["sup3r_se3cr3t"],
     path: "/",
     httpOnly: true,
-    sameSite: process.env.NODE_ENV === "development" ? false : "lax",
+    sameSite: "none",
     maxAge: 120, // 2 minutes
   },
 });
